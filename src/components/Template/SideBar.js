3e3ec5d9b031ebc0393908345ea448ce79bdf2e6
sandhyadeepch <sandhyadeepch@gmail.com>
2023-02-12 17:29:23 -0600

    logo extension uppercase change

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,7 +9,7 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+        <img src={`${PUBLIC_URL}/images/me.JPG`} alt="" />
       </Link>
       <header>
         <h2>Sandhya R Marri</h2>
